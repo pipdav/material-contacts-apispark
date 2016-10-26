23991cf3183167ee07c6355f3fcf9e043966d22b
David Hartke <hartke.david@gmail.com>
2016-10-26 16:47:07 +0200

    added new dummy test

--- src/test/java/net/apispark/webapi/DummyTest.java
+++ src/test/java/net/apispark/webapi/DummyTest.java
@@ -15,4 +15,9 @@ public class DummyTest {
     public void dummy_assertion() throws Exception {
         Assert.assertThat("test is good", is(instanceOf(String.class)));
     }
+    
+    @Test
+    public void dummy_assertion_2() throws Exception {
+        Assert.assertThat(1, is(instanceOf(Integer.class)));
+    }
 }
